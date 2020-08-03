49d5255fcb820678c7b9cc0d34829916022f31b0
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-08-03 03:53:04 +0100

    yarn dev clear terminal on start and on file save

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,4 +3,6 @@ import app from './app'
 
 const port = process.env.PORT || 3030
 
-app.listen(port)
+app.listen(port, () => {
+	console.log(`🔥 Server up and runing on http://localhost:${port}/ 🥇`)
+})
