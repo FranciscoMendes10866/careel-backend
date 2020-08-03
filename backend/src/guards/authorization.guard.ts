453c046c9f948578cc92d638950a7123fdf794c0
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-08-03 03:03:41 +0100

    created token verification guard

---
+++ backend/src/guards/authorization.guard.ts
@@ -0,0 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const authorization = async (ctx, next) => {
+	// Get auth header value
+	const bearerHeader = ctx.header.authorization
+	// Check if bearer is undefined
+	if (typeof bearerHeader !== 'undefined') {
+		// Split at the space
+		const bearer = bearerHeader.split(' ')
+		// Get token from array
+		const bearerToken = bearer[1]
+		// Set the token
+		ctx.token = bearerToken
+		// Next middleware
+		next()
+	} else {
+		// error
+		ctx.throw(401, 'Protected resource, access denied.')
+	}
+}
+  
+export default authorization
